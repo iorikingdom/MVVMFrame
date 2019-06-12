66dd1446990fac591c877683acae435f8a4145c4
wangxxx <764690578@qq.com>
2019-06-12 14:31:39 +0800

    修改radiogroup adapter

--- MVVMFrame/src/main/java/com/wangxing/code/mvvm/binding/viewadapter/radiogroup/ViewAdapter.java
+++ MVVMFrame/src/main/java/com/wangxing/code/mvvm/binding/viewadapter/radiogroup/ViewAdapter.java
@@ -10,13 +10,13 @@ import com.wangxing.code.mvvm.binding.command.BindingCommand;
 
 public class ViewAdapter {
     @BindingAdapter(value = {"onCheckedChangedCommand"}, requireAll = false)
-    public static void onCheckedChangedCommand(final RadioGroup radioGroup, final BindingCommand<String> bindingCommand) {
+    public static void onCheckedChangedCommand(final RadioGroup radioGroup, final BindingCommand<RadioButton> bindingCommand) {
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
-                RadioButton radioButton = (RadioButton) group.findViewById(checkedId);
-                bindingCommand.execute(radioButton.getText().toString());
+                bindingCommand.execute((RadioButton) group.findViewById(checkedId));
             }
         });
+
     }
 }
