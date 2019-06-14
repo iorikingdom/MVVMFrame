c74b327654ac7e872aa2c48072395fb47e17ccc4
wangxxx <764690578@qq.com>
2019-06-14 15:07:25 +0800

    更改itemview里viewmodel修饰词

--- MVVMFrame/src/main/java/com/wangxing/code/mvvm/base/ItemViewModel.java
+++ MVVMFrame/src/main/java/com/wangxing/code/mvvm/base/ItemViewModel.java
@@ -6,7 +6,7 @@ import android.support.annotation.NonNull;
  * Created by WangXing on 2019/5/28.
  */
 public class ItemViewModel<VM extends BaseViewModel> {
-    private VM viewModel;
+    protected VM viewModel;
 
     public ItemViewModel(@NonNull VM viewModel) {
         this.viewModel = viewModel;
