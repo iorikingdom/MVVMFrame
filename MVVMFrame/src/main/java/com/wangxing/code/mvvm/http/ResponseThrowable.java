b6b9f167080754a7ff6af5fb2e8d10bfcbde051c
wangxxx <764690578@qq.com>
2019-07-02 14:35:51 +0800

    ResponseThrowable增加处理code方法

--- MVVMFrame/src/main/java/com/wangxing/code/mvvm/http/ResponseThrowable.java
+++ MVVMFrame/src/main/java/com/wangxing/code/mvvm/http/ResponseThrowable.java
@@ -2,6 +2,8 @@ package com.wangxing.code.mvvm.http;
 
 import com.wangxing.code.mvvm.utils.ToastUtils;
 
+import java.util.Map;
+
 /**
  * Created by WangXing on 2019/5/28.
  */
@@ -19,6 +21,11 @@ public class ResponseThrowable extends Exception {
         if (null != message) {
             ToastUtils.showShort(message);
         }
+    }
+
+    public void handleCode(Map<Integer, String> map) {
+        ToastUtils.showShort(map.get(code));
+
     }
 
 }
