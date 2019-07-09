8eb058b1348b2604aa54e5cb6c0f3401551515f0
wangxxx <764690578@qq.com>
2019-07-09 13:21:25 +0800

    修改Application

--- MVVMFrame/src/main/java/com/wangxing/code/mvvm/base/BaseApplication.java
+++ MVVMFrame/src/main/java/com/wangxing/code/mvvm/base/BaseApplication.java
@@ -19,7 +19,6 @@ import java.lang.reflect.Field;
 public class BaseApplication extends Application {
 
     private static Application sInstance;
-    public static int http_loading = 0;
 
     @Override
     public void onCreate() {
@@ -30,27 +29,6 @@ public class BaseApplication extends Application {
                 .supportBroadcast(this)
                 .lifecycleObserverAlwaysActive(true);
         setApplication(this);
-
-        Class<?> aClass = null;
-        try {
-            aClass = Class.forName(ContextUtils.getContext().getPackageName() + ".Const");
-            Field httpLoading = aClass.getDeclaredField("HTTP_LOADING");
-            http_loading = Integer.parseInt(httpLoading.get(aClass).toString());
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void setHttpLoading(@LayoutRes int layoutRes) {
-        http_loading = layoutRes;
-    }
-
-    public static int getHttpLoading() {
-        return http_loading;
     }
 
     /**
@@ -97,22 +75,6 @@ public class BaseApplication extends Application {
         });
 
     }
-
-    //初始化全局异常崩溃
-//    private void initCrash() {
-//        CaocConfig.Builder.create()
-//                .backgroundMode(CaocConfig.BACKGROUND_MODE_SILENT) //背景模式,开启沉浸式
-//                .enabled(true) //是否启动全局异常捕获
-//                .showErrorDetails(true) //是否显示错误详细信息
-//                .showRestartButton(true) //是否显示重启按钮
-//                .trackActivities(true) //是否跟踪Activity
-//                .minTimeBetweenCrashesMs(2000) //崩溃的间隔时间(毫秒)
-//                .errorDrawable(R.mipmap.ic_launcher) //错误图标
-//                .restartActivity(LoginActivity.class) //重新启动后的activity
-//                .errorActivity(YourCustomErrorActivity.class) //崩溃后的错误activity
-//                .eventListener(new YourCustomEventListener()) //崩溃后的错误监听
-//                .apply();
-//    }
 
     /**
      * 获得当前app运行的Application
